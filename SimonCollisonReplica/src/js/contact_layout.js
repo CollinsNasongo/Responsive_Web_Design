9a3c579002e6a003a090ff0f5746ee6921f927d0
CollinsNasongo <nasongocollins@gmail.com>
2024-03-26 13:04:26 +0300

    Contact Page

---
+++ SimonCollisonReplica/src/js/contact_layout.js
@@ -0,0 +1,6 @@
+(function() {
+  var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
+Handlebars.partials['contact_layout'] = template({"compiler":[8,">= 4.3.0"],"main":function(container,depth0,helpers,partials,data) {
+    return "<h1 class=\"text-4xl font-semibold mt-14 mb-6 log-color\">Contact me</h1>\r\n<p class=\"my-6 text-xl font-semibold\">I’m always interested in new opportunities — and I’m friendly, so don’t hesitate to get in touch.</p>\r\n<p class=\"my-6 text-xl font-semibold log-color\"><a class=\"page-link\" href=\"\">simoncollison@gmail.com</a></p>\r\n<p class=\"my-6 text-xl font-semibold\">Thanks!</p>";
+},"useData":true});
+})();
